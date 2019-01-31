61a7883de9e650113810f4e85064d442a1e4e18e
diemnvvn <diemnvvn@gmail.com>
2019-01-31 17:47:45 +0700

    diemnv frontend

---
+++ src/app/semester-exam/admin/semester-create/semester-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-semester-create',
+  templateUrl: './semester-create.component.html',
+  styleUrls: ['./semester-create.component.css']
+})
+export class SemesterCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/semester-exam/admin/semester-detail/semester-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-semester-detail',
+  templateUrl: './semester-detail.component.html',
+  styleUrls: ['./semester-detail.component.css']
+})
+export class SemesterDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
