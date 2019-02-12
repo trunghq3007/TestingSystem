92fb7472cbed87f9bdfc003ed51c5e6a487372d1
dinhthuan <dinhthuanvan@gmail.com>
2019-02-12 17:54:37 +0700

    no message

--- src/app/semester-exam/semester/create/create.component.ts
+++ src/app/semester-exam/semester/create/create.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+   selector: 'app-create',
+   templateUrl: './create.component.html',
+   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+   ckeConfig: any;
+   log: string = '';
+   @ViewChild("myckeditor") ckeditor: any;
 
-  ngOnInit() {
-  }
+   constructor() {
+   }
+
+   ngOnInit() {
+      this.ckeConfig = {
+         allowedContent: false,
+         extraPlugins: 'divarea',
+         forcePasteAsPlainText: true
+      };
+   }
+
+   onChange(event: any): void {
+      console.log(event);
+   }
 
 }
--- src/app/semester-exam/semester/semester.module.ts
+++ src/app/semester-exam/semester/semester.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CKEditorModule } from 'ng2-ckeditor';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
 import { SemesterRoutingModule } from './semester-routing.module';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
@@ -10,12 +13,22 @@ import { InfoComponent } from './detail/info/info.component';
 import { ReportComponent } from './detail/report/report.component';
 import { CandidateComponent } from './detail/candidate/candidate.component';
 import { ExamComponent } from './detail/exam/exam.component';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatNativeDateModule } from '@angular/material';
+
 
 @NgModule({
-  declarations: [ListComponent, DetailComponent, EditComponent, CreateComponent, InfoComponent, ReportComponent, CandidateComponent, ExamComponent],
-  imports: [
-    CommonModule,
-    SemesterRoutingModule
-  ]
+   declarations: [ListComponent, DetailComponent, EditComponent, CreateComponent, InfoComponent, ReportComponent, CandidateComponent, ExamComponent],
+   imports: [
+      CommonModule,
+      SemesterRoutingModule,
+      CKEditorModule,
+      MatDatepickerModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatRippleModule,
+      MatDatepickerModule,
+      MatNativeDateModule
+   ]
 })
 export class SemesterModule { }
