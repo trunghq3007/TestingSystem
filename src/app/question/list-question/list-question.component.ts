03267f525cd44402af289c2e8ec2252c09727413
vancuong <ngoccuongbich6972@gmail.com>
2019-02-13 13:19:08 +0700

    update sevice

--- src/app/question/list-question/list-question.component.ts
+++ src/app/question/list-question/list-question.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Tag } from 'src/entity/Tag';
 import {v4 as uuid} from 'uuid';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -19,14 +20,17 @@ export class ListQuestionComponent implements OnInit {
               private router: Router ) { 
               }
  ngOnInit() {
+  //  tag class and validate
    this.tagFrm=this.fb.group({
      tag_name:['',[Validators.required, Validators.minLength(3),Validators.maxLength(225)]],
      description:['',[Validators.required, Validators.minLength(10), Validators.maxLength(225)]],
      status:['',[Validators.required, Validators.minLength(1), Validators.maxLength(11)]]
    });
  }
+//  tag mesage sucess
 message=false;
  onSubmit(){
+  //  tag add + auto generate id
    if(this.tagFrm.value){
     const value=this.tagFrm.value;
      const tag:Tag = {
@@ -35,6 +39,8 @@ message=false;
     };
     this.http.post('http://localhost:3000/tag',tag).subscribe(()=>{this.router.navigateByUrl('/tag');});
     this.message = true;
+    
  }
 }
+
 }
