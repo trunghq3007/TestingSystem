2b96ab74a9bd436cd2429a4022210d2eda125517
diemnvvn <diemnvvn@gmail.com>
2019-02-13 09:11:18 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { SemesterExamModule } from './semester-exam/semester-exam.module';
 
 const routes: Routes = [
-  {
-    path: 'manager',
-    children: [
-      {
-        path: 'question',
-        pathMatch: 'full', redirectTo: '/question'
-      },
-    ]
-  },
-  {
-    path: '', pathMatch: 'full', redirectTo: '/home'
-  }
+   {
+      path: 'manager',
+      children: [
+         {
+            path: 'question',
+            pathMatch: 'full', redirectTo: '/question'
+         },
+      ]
+   },
+   {
+      path: '', pathMatch: 'full', redirectTo: '/home'
+   }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    SemesterExamModule],
-  exports: [RouterModule]
+   imports: [
+      RouterModule.forRoot(routes),
+      SemesterExamModule],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
