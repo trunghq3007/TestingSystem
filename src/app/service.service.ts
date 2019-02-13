36bb0aac375a52f67ee0526350f1886fd679c800
vancuong <ngoccuongbich6972@gmail.com>
2019-02-13 11:33:12 +0700

    update sevice

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/tag';
+
+  constructor(private http: HttpClient) { }
+
+  getTag(id: number): Observable<Object> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createTag(tag: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}` + `/create`, tag);
+  }
+  //abc
 
 }
