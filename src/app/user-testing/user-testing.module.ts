c18c0a6fbba88ecc648d8ca27964339a19745a94
diemnvvn <diemnvvn@gmail.com>
2019-02-15 10:57:59 +0700

    diemnv commit 10:57 15-2-2019

--- src/app/user-testing/user-testing.module.ts
+++ src/app/user-testing/user-testing.module.ts
@@ -4,13 +4,15 @@ import { UserTestingComponent } from './user-testing.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { UserTestingRoutingModule } from './user-testing-routing.module';
 import { UserTestingResultComponent } from './user-testing-result/user-testing-result.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
    declarations: [UserTestingComponent, UserTestingResultComponent],
    imports: [
       CommonModule,
       ModalModule.forRoot(),
-      UserTestingRoutingModule
+      UserTestingRoutingModule,
+      RouterModule
    ],
    exports: [UserTestingComponent]
 })
