0f2cd766491b29c1fdeb3fd61efbe3978e407c5f
dinhthuan <dinhthuanvan@gmail.com>
2019-02-15 14:28:39 +0700

    14h28 - 15/2/2019 - dvthuan

--- src/app/semester-exam/semester/list/list.component.ts
+++ src/app/semester-exam/semester/list/list.component.ts
@@ -8,17 +8,47 @@ import { SemesterExam } from '../../models/SemesterExam';
    styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+   keyword: string = "";
    selectedAll: any;
+   id: string;
    public semesterExamList: SemesterExam;
 
    constructor(private service: ApiService) { }
 
+
+
+   enter(event) {
+      if (event.key == "Enter") {
+         this.searchByKeyword();
+      }
+   }
+
    getId(event: any) {
       console.log(event);
+      this.id = event;
    }
 
    deleteItem() {
+      var temp = confirm("Bạn có chắc chắn muốn xóa không?");
+      if (temp) {
+         this.service.delete('semesterexam/delete', this.id).subscribe(result => {
+            console.log(result.data);
+            this.semesterExamList = result.data;
+         });
+      }
+   }
 
+   searchByKeyword() {
+      if (this.keyword == "") {
+         this.service.getAll('semesterexam/all').subscribe(result => {
+            this.semesterExamList = result.data;
+            console.log(this.semesterExamList);
+         });
+      } else {
+         this.service.search('semesterexam/search', this.keyword).subscribe(result => {
+            this.semesterExamList = result.data;
+         });
+      }
    }
 
    ngOnInit() {
