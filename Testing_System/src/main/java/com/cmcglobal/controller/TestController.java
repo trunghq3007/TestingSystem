b4ea504226ac341d73fffa30e8dd9b01847c176f
VuThuy1995 <vuthuy.td95@gmail.com>
2019-02-20 17:40:35 +0700

    change test

--- Testing_System/src/main/java/com/cmcglobal/controller/TestController.java
+++ Testing_System/src/main/java/com/cmcglobal/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -81,8 +82,8 @@ public class TestController {
       @param id
       @return
      */
-    @PostMapping("/delete")
-    public ResponseEntity<ServiceResult> deleteTest(@RequestBody Integer id) {
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<ServiceResult> deleteTest(@PathVariable Integer id) {
         return new ResponseEntity<ServiceResult>(testService.deleteTest(id), HttpStatus.OK);
     }
 }
--- Testing_System/src/main/java/com/cmcglobal/entity/Test.java
+++ Testing_System/src/main/java/com/cmcglobal/entity/Test.java
@@ -18,7 +18,7 @@ public class Test implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "test_id")
 	private int testID;
 	@Column(name = "test_name")
