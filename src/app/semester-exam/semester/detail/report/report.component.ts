63a05748d66fb9dd04986e6c6dc72564aefe8c5a
haohus <phamhao96.hus@gmail.com>
2019-02-21 11:50:54 +0700

    pvhao commit 11h50 21-2

--- src/app/semester-exam/semester/detail/report/report.component.ts
+++ src/app/semester-exam/semester/detail/report/report.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/semester-exam/service/api.service';
+import { drawDOM, exportPDF, DrawOptions, Group } from '@progress/kendo-drawing';
+import { saveAs } from '@progress/kendo-file-saver';
 @Component({
    selector: 'app-report',
    templateUrl: './report.component.html',
-   styleUrls: ['./report.component.css']
+   styleUrls: ['./report.component.css','./pdf-styles.css']
 })
 export class ReportComponent implements OnInit {
 
    @Input()
-   semesterExamCode:string;
+   semesterExamCode: string;
    data = Object;
+   title_file : string;
+   options = {
+      paperSize: "A4",
+      repeatHeaders: true,
+      scale: 0.8,
+      margin: {
+         top: "80pt"
+      },
+   };
+   @ViewChild("exportPDF", { read: ElementRef }) htmlPDF: ElementRef;
    constructor(private apiService: ApiService) { }
 
    ngOnInit() {
@@ -19,12 +31,19 @@ export class ReportComponent implements OnInit {
       this.apiService.getData(`semesterexam/info/${this.semesterExamCode}`).subscribe(
          result => {
             this.data = result;
-            // console.log
+            this.title_file = result.semesterExam.name;
          },
          error => {
-
          }
       );
    }
-
+   onExport() {
+     // this.htmlPDF.nativeElement.setAttribute("class", "show");
+      drawDOM(this.htmlPDF.nativeElement, this.options).then((group: Group) => {
+        // this.htmlPDF.nativeElement.setAttribute("class", "hidden");
+         return exportPDF(group);
+      }).then((dataUri) => {
+         saveAs(dataUri, `${this.title_file}.pdf`);
+      });
+   }
 }
