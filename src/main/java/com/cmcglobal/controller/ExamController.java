fcb1d22201fcdf49b517f648cce9148f2a7f0f75
vsson <sonsbe1997@gmail.com>
2019-02-22 11:56:52 +0700

    fix exam has no question

--- src/main/java/com/cmcglobal/controller/ExamController.java
+++ src/main/java/com/cmcglobal/controller/ExamController.java
@@ -1,8 +1,11 @@
 package com.cmcglobal.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,251 +22,262 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.cmcglobal.entity.Exam;
 import com.cmcglobal.entity.User;
 import com.cmcglobal.repository.CategoryRepository;
 import com.cmcglobal.service.ExamService;
-import com.cmcglobal.utils.ExportExamPDF;
-import java.io.File;
-import java.io.IOException;
-import org.springframework.web.multipart.MultipartFile;
 import com.cmcglobal.utils.Api;
+import com.cmcglobal.utils.ExportExamPDF;
 
 @RestController
 @RequestMapping(Api.Exam.BASE_URL)
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class ExamController {
-	@Autowired
-	ExamService examService;
-	@Autowired
-	CategoryRepository cate;
+  @Autowired
+  ExamService examService;
+  @Autowired
+  CategoryRepository cate;
 
-	@PostMapping(value = "/create")
-	public void postExam(@RequestBody Exam exam) {
-		examService.createExam(exam);
-	}
+  @PostMapping(value = "/create")
+  public void postExam(@RequestBody Exam exam) {
+    examService.createExam(exam);
+  }
 
-	@GetMapping(value = "/listExams")
-	public List<Exam> listExam() {
-		return examService.findAll();
-	}
+  @GetMapping(value = "/listExams")
+  public List<Exam> listExam() {
+    return examService.findAll();
+  }
 
-	@RequestMapping(value = "listExams/pagination", method = RequestMethod.GET)
-	private List<Exam> getPageExam(
-			@RequestParam(name = "sortOrder", required = false, defaultValue = "ASC") String sortOrder,
-			@RequestParam(name = "sortTerm", required = false, defaultValue = "title") String sortTerm,
-			@RequestParam(name = "searchContent", required = false, defaultValue = "") String searchContent) {
-		Pageable sortedBy = null;
-		Sort sortable = null;
-		switch (sortTerm) {
-		case ("title"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("title").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("title").descending();
-			}
-			break;
-		case ("examId"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("examId").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("examId").descending();
-			}
-			break;
-		case ("duration"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("duration").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("duration").descending();
-			}
-			break;
-		case ("numberOfQuestion"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("numberOfQuestion").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("numberOfQuestion").descending();
-			}
-			break;
-		case ("status"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("status").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("status").descending();
-			}
-			break;
-		case ("createAt"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("createAt").ascending();
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				sortable = Sort.by("createAt").descending();
-			}
-			break;
-		// sort theo trường fullname của user create_by
-		case ("userCreated"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				return examService.pageExamSortByUserCreatedByAsc(searchContent);
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				return examService.pageExamSortByUserCreatedByDesc(searchContent);
-			}
-			break;
-		// sort theo trường category của category category_name
-		case ("category"):
-			if (("asc").equals(sortOrder.toLowerCase())) {
-				return examService.pageExamSortByCategoryAsc(searchContent);
-			}
-			if (("desc").equals(sortOrder.toLowerCase())) {
-				return examService.pageExamSortByCategoryDesc(searchContent);
-			}
-			break;
-		}
-		return examService.pageExam(searchContent, sortable);
-	}
+  @RequestMapping(value = "listExams/pagination", method = RequestMethod.GET)
+  private List<Exam> getPageExam(
+      @RequestParam(name = "sortOrder", required = false, defaultValue = "ASC") String sortOrder,
+      @RequestParam(name = "sortTerm", required = false, defaultValue = "title") String sortTerm,
+      @RequestParam(name = "searchContent", required = false, defaultValue = "") String searchContent) {
+    Pageable sortedBy = null;
+    Sort sortable = null;
+    switch (sortTerm) {
+    case ("title"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("title").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("title").descending();
+      }
+      break;
+    case ("examId"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("examId").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("examId").descending();
+      }
+      break;
+    case ("duration"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("duration").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("duration").descending();
+      }
+      break;
+    case ("numberOfQuestion"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("numberOfQuestion").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("numberOfQuestion").descending();
+      }
+      break;
+    case ("status"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("status").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("status").descending();
+      }
+      break;
+    case ("createAt"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("createAt").ascending();
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        sortable = Sort.by("createAt").descending();
+      }
+      break;
+    // sort theo trường fullname của user create_by
+    case ("userCreated"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        return examService.pageExamSortByUserCreatedByAsc(searchContent);
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        return examService.pageExamSortByUserCreatedByDesc(searchContent);
+      }
+      break;
+    // sort theo trường category của category category_name
+    case ("category"):
+      if (("asc").equals(sortOrder.toLowerCase())) {
+        return examService.pageExamSortByCategoryAsc(searchContent);
+      }
+      if (("desc").equals(sortOrder.toLowerCase())) {
+        return examService.pageExamSortByCategoryDesc(searchContent);
+      }
+      break;
+    }
+    return examService.pageExam(searchContent, sortable);
+  }
 
-	@GetMapping(value = "/export/{id}")
-	public ModelAndView handlereport(@PathVariable("id") String id) {
-		try {
-			Exam exam = examService.findByID(id);
-			return new ModelAndView(new ExportExamPDF(), "exam", exam);
-		} catch (Exception e) {
-			return null;
-		}
-	}
+  @GetMapping(value = "/export/{id}")
+  public ModelAndView handlereport(@PathVariable("id") String id) {
+    try {
+      Exam exam = examService.findByID(id);
+      return new ModelAndView(new ExportExamPDF(), "exam", exam);
+    } catch (Exception e) {
+      return null;
+    }
+  }
 
-	@GetMapping(value = "/{id}")
-	public Exam getExam(@PathVariable("id") String id) {
-		/*
-		 * cate.delete(cate.getOne(1)); cate.deleteAll();
-		 */
-		return examService.findByID(id);
-	}
+  @GetMapping(value = "/{id}")
+  public Exam getExam(@PathVariable("id") String id) {
+    /*
+     * cate.delete(cate.getOne(1)); cate.deleteAll();
+     */
+    return examService.findByID(id);
+  }
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:03:02 PM
-	 * Description: TODO - controller handle approve exam to public.
-	 * 
-	 * @param exam
-	 * @return
-	 */
-	@PutMapping(value = Api.Exam.APPROVE)
-	public ResponseEntity<String> approveExam(@RequestBody Exam exam) {
-		boolean success = examService.approveExam(exam.getExamId());
-		if (success)
-			return ResponseEntity.ok(Api.Exam.OK);
-		return ResponseEntity.ok(Api.Exam.NOT_OK);
-	}
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:03:02 PM
+   * Description: TODO - controller handle approve exam to public.
+   * 
+   * @param exam
+   * @return
+   */
+  @PutMapping(value = Api.Exam.APPROVE)
+  public ResponseEntity<String> approveExam(@RequestBody Exam exam) {
+    boolean success = examService.approveExam(exam.getExamId());
+    if (success)
+      return ResponseEntity.ok(Api.Exam.OK);
+    return ResponseEntity.ok(Api.Exam.NOT_OK);
+  }
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:52 PM
-	 * Description: TODO - controller handle remove question to exam.
-	 * 
-	 * @param exam
-	 * @return
-	 */
-	@PutMapping(value = Api.Exam.REMOVE_QUESTION)
-	public ResponseEntity<String> removeQuestion(@RequestBody Exam exam) {
-		boolean success = examService.removeQuestion(exam);
-		if (success)
-			return ResponseEntity.ok(Api.Exam.OK);
-		return ResponseEntity.ok(Api.Exam.NOT_OK);
-	}
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:52 PM
+   * Description: TODO - controller handle remove question to exam.
+   * 
+   * @param exam
+   * @return
+   */
+  @PutMapping(value = Api.Exam.REMOVE_QUESTION)
+  public ResponseEntity<String> removeQuestion(@RequestBody Exam exam) {
+    boolean success = examService.removeQuestion(exam);
+    if (success)
+      return ResponseEntity.ok(Api.Exam.OK);
+    return ResponseEntity.ok(Api.Exam.NOT_OK);
+  }
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:45 PM
-	 * Description: TODO - controller handle add question to exam.
-	 * 
-	 * @param exam
-	 * @return
-	 */
-	@PostMapping(value = Api.Exam.ADD_QUESTION)
-	public ResponseEntity<String> addQuestion(@RequestBody Exam exam) {
-		boolean success = examService.addListQuestion(exam);
-		if (success)
-			return ResponseEntity.ok(Api.Exam.OK);
-		return ResponseEntity.ok(Api.Exam.NOT_OK);
-	}
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:45 PM
+   * Description: TODO - controller handle add question to exam.
+   * 
+   * @param exam
+   * @return
+   */
+  @PostMapping(value = Api.Exam.ADD_QUESTION)
+  public ResponseEntity<String> addQuestion(@RequestBody Exam exam) {
+    boolean success = examService.addListQuestion(exam);
+    if (success)
+      return ResponseEntity.ok(Api.Exam.OK);
+    return ResponseEntity.ok(Api.Exam.NOT_OK);
+  }
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:29 PM
-	 * Description: TODO - controller handle random question to exam.
-	 * 
-	 * @param exam
-	 * @return
-	 */
-	@PostMapping(value = Api.Exam.RANDOM_QUESTION)
-	public ResponseEntity<String> randomQuestion(@RequestBody Exam exam) {
-		boolean success = examService.randomQuestion(exam.getExamId(), exam.getNumberOfQuestion());
-		if (success)
-			return ResponseEntity.ok(Api.Exam.OK);
-		return ResponseEntity.ok(Api.Exam.NOT_OK);
-	}
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:02:29 PM
+   * Description: TODO - controller handle random question to exam.
+   * 
+   * @param exam
+   * @return
+   */
+  @PostMapping(value = Api.Exam.RANDOM_QUESTION)
+  public ResponseEntity<String> randomQuestion(@RequestBody Exam exam) {
+    boolean success = examService.randomQuestion(exam.getExamId(),
+        exam.getNumberOfQuestion());
+    if (success)
+      return ResponseEntity.ok(Api.Exam.OK);
+    return ResponseEntity.ok(Api.Exam.NOT_OK);
+  }
 
-	@DeleteMapping(value = "/{examId}")
-	public void deleteExam(@PathVariable String examId) {
-		examService.deleteExam(examId);
-	}
+  @DeleteMapping(value = "/{examId}")
+  public void deleteExam(@PathVariable String examId) {
+    examService.deleteExam(examId);
+  }
 
-	@PostMapping(value = "/filter")
-	public ResponseEntity<List<Exam>> findAll(@RequestBody Exam exam) {
-		List<Exam> exams = examService.FilterExam(exam);
-		return ResponseEntity.ok(exams);
-	}
+  @PostMapping(value = "/filter")
+  public ResponseEntity<List<Exam>> findAll(@RequestBody Exam exam) {
+    List<Exam> exams = examService.FilterExam(exam);
+    return ResponseEntity.ok(exams);
+  }
 
-	@PutMapping("/update/update-common/{examId}")
-	public Exam updateCommon(@RequestBody Exam exam, @PathVariable String examId) {
-		Exam ex = examService.getOne(examId);
-		if (ex != null) {
-			ex.setTitle(exam.getTitle());
-			ex.setCategory(exam.getCategory());
-			ex.setDuration(exam.getDuration());
-			ex.setNumberOfQuestion(exam.getNumberOfQuestion());
-			ex.setNote(exam.getNote());
-			ex.setStatus(exam.getStatus());
-			examService.update(ex);
-			System.out.println(exam.getCategory().getCategoryName());
-		}
-		return ex;
-	}
+  @PutMapping("/update/update-common/{examId}")
+  public Exam updateCommon(@RequestBody Exam exam,
+      @PathVariable String examId) {
+    Exam ex = examService.getOne(examId);
+    if (ex != null) {
+      ex.setTitle(exam.getTitle());
+      ex.setCategory(exam.getCategory());
+      ex.setDuration(exam.getDuration());
+      ex.setNumberOfQuestion(exam.getNumberOfQuestion());
+      ex.setNote(exam.getNote());
+      ex.setStatus(exam.getStatus());
+      examService.update(ex);
+      System.out.println(exam.getCategory().getCategoryName());
+    }
+    return ex;
+  }
 
-	@PostMapping("/import-excel-file")
-	public ResponseEntity<String> readExcelFile(@RequestParam("multipartFile") MultipartFile multipartFile) {
+  @PostMapping("/import-excel-file")
+  public ResponseEntity<String> readExcelFile(
+      @RequestParam("multipartFile") MultipartFile multipartFile) {
 
-		File file = new File("files");
-		String pathToSave = file.getAbsolutePath();
-		System.out.println(pathToSave);
+    File file = new File("files");
+    String pathToSave = file.getAbsolutePath();
+    System.out.println(pathToSave);
 
-		File fileTranfer = new File(pathToSave + "/" + multipartFile.getOriginalFilename());
-		try {
-			multipartFile.transferTo(fileTranfer);
-		} catch (IllegalStateException e) {
-		} catch (IOException e) {
-		}
-		System.out.println(multipartFile.getOriginalFilename());
-		List<Exam> listExam = new ArrayList<>();
-		try {
-			listExam = examService.readExcel(fileTranfer.toString());
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.OK).body("not Ok");
-		}
-		if (listExam.size() == 0) {
-			return ResponseEntity.status(HttpStatus.OK).body("not Ok");
-		}
-		for (Exam exam : listExam) {
-			exam.setExamId(examService.createId1());
-			User user = new User();
-			user.setUserId(1);
-			exam.setUserCreated(user);
-			exam.setStatus("Draft");
-			exam.setCreateAt(new Date());
-			examService.insert(exam);
-		}
-		return ResponseEntity.status(HttpStatus.OK).body("Ok");
-	}
+    File fileTranfer = new File(
+        pathToSave + "/" + multipartFile.getOriginalFilename());
+    try {
+      multipartFile.transferTo(fileTranfer);
+    } catch (IllegalStateException e) {
+    } catch (IOException e) {
+    }
+    System.out.println(multipartFile.getOriginalFilename());
+    List<Exam> listExam = new ArrayList<>();
+    try {
+      listExam = examService.readExcel(fileTranfer.toString());
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.OK).body("not Ok");
+    }
+    if (listExam.size() == 0) {
+      return ResponseEntity.status(HttpStatus.OK).body("not Ok");
+    }
+    for (Exam exam : listExam) {
+      exam.setExamId(examService.createId1());
+      User user = new User();
+      user.setUserId(1);
+      exam.setUserCreated(user);
+      exam.setStatus("Draft");
+      exam.setCreateAt(new Date());
+      examService.insert(exam);
+    }
+    return ResponseEntity.status(HttpStatus.OK).body("Ok");
+  }
+
+  @GetMapping(value = Api.Exam.IS_EMPTY_QUESTION)
+  public ResponseEntity<String> isEmptyQuestion(@PathVariable String examId) {
+    boolean success = examService.isEmptyQuestionOfExam(examId);
+    System.out.println(examId);
+    if (success) return ResponseEntity.ok(Api.Exam.OK);
+    return ResponseEntity.ok(Api.Exam.NOT_OK);
+  }
 }
--- src/main/java/com/cmcglobal/service/ExamService.java
+++ src/main/java/com/cmcglobal/service/ExamService.java
@@ -8,95 +8,105 @@ import java.util.List;
 import com.cmcglobal.entity.Exam;
 
 public interface ExamService {
-	/**
-	 * Author: ntmduyen. Created date: Feb 20, 2019 Created time: 12:41:55 PM
-	 * Description: TODO - .
-	 * 
-	 * @return
-	 */
-	public List<Exam> findAll();
+  /**
+   * Author: ntmduyen. Created date: Feb 20, 2019 Created time: 12:41:55 PM
+   * Description: TODO - .
+   * 
+   * @return
+   */
+  public List<Exam> findAll();
 
-	public List<Exam> pageExam(String searchContent, Sort pageable);
+  public List<Exam> pageExam(String searchContent, Sort pageable);
 
-	public List<Exam> pageExamSortByUserCreatedByAsc(String searchContent);
+  public List<Exam> pageExamSortByUserCreatedByAsc(String searchContent);
 
-	public List<Exam> pageExamSortByUserCreatedByDesc(String searchContent);
+  public List<Exam> pageExamSortByUserCreatedByDesc(String searchContent);
 
-	public List<Exam> pageExamSortByCategoryAsc(String searchContent);
+  public List<Exam> pageExamSortByCategoryAsc(String searchContent);
 
-	public List<Exam> pageExamSortByCategoryDesc(String searchContent);
+  public List<Exam> pageExamSortByCategoryDesc(String searchContent);
 
-	public Exam findByID(String id);
+  public Exam findByID(String id);
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:01:15 PM
-	 * Description: TODO - approve exam to public.
-	 * 
-	 * @param examId
-	 * @return
-	 */
-	public boolean approveExam(String examId);
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:01:15 PM
+   * Description: TODO - approve exam to public.
+   * 
+   * @param examId
+   * @return
+   */
+  public boolean approveExam(String examId);
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:00:44 PM
-	 * Description: TODO - random question to exam.
-	 * 
-	 * @param examId       - exam id.
-	 * @param numberRandom - number of random question.
-	 * @return
-	 */
-	public boolean randomQuestion(String examId, int numberRandom);
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:00:44 PM
+   * Description: TODO - random question to exam.
+   * 
+   * @param examId       - exam id.
+   * @param numberRandom - number of random question.
+   * @return
+   */
+  public boolean randomQuestion(String examId, int numberRandom);
 
-	/**
-	 * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:00:26 PM
-	 * Description: TODO - remove question from exam.
-	 * 
-	 * @param exam
-	 * @return
-	 */
-	public boolean removeQuestion(Exam exam);
+  /**
+   * Author: Sanero. Created date: Feb 19, 2019 Created time: 4:00:26 PM
+   * Description: TODO - remove question from exam.
+   * 
+   * @param exam
+   * @return
+   */
+  public boolean removeQuestion(Exam exam);
 
-	/**
-	 * Author: Sanero. Created date: Feb 14, 2019 Created time: 8:35:49 AM
-	 * Description: TODO - add list question to exam.
-	 * 
-	 * @param exam
-	 */
-	public boolean addListQuestion(Exam exam);
+  /**
+   * Author: Sanero. Created date: Feb 14, 2019 Created time: 8:35:49 AM
+   * Description: TODO - add list question to exam.
+   * 
+   * @param exam
+   */
+  public boolean addListQuestion(Exam exam);
 
-	/**
-	 * Author: ptphuong. Created date: Feb 15, 2019 Created time: 5:22:39 AM
-	 * Description: TODO - .
-	 * 
-	 * @param ex
-	 */
-	public void createExam(Exam ex);
+  /**
+   * Author: ptphuong. Created date: Feb 15, 2019 Created time: 5:22:39 AM
+   * Description: TODO - .
+   * 
+   * @param ex
+   */
+  public void createExam(Exam ex);
 
-	/**
-	 * Author: ptphuong. Created date: Feb 15, 2019 Created time: 7:55:39 PM
-	 * Description: TODO - .
-	 * 
-	 * @return
-	 */
-	public String createId();
+  /**
+   * Author: ptphuong. Created date: Feb 15, 2019 Created time: 7:55:39 PM
+   * Description: TODO - .
+   * 
+   * @return
+   */
+  public String createId();
 
-	/**
-	 * Author: ndvan. Created date: Feb 15, 2019 Created time: 5:22:39 AM
-	 * Description: TODO - .
-	 * 
-	 * @param ex
-	 */
-	public void deleteExam(String examId);
+  /**
+   * Author: ndvan. Created date: Feb 15, 2019 Created time: 5:22:39 AM
+   * Description: TODO - .
+   * 
+   * @param ex
+   */
+  public void deleteExam(String examId);
 
-	public List<Exam> FilterExam(Exam exam);
+  public List<Exam> FilterExam(Exam exam);
 
-	public String createId1();
+  public String createId1();
 
-	Exam getOne(String examId);
+  Exam getOne(String examId);
 
-	Exam insert(Exam exam);
+  Exam insert(Exam exam);
 
-	Exam update(Exam exam);
+  Exam update(Exam exam);
 
-	List<Exam> readExcel(String exelFilePath) throws Exception;
+  List<Exam> readExcel(String exelFilePath) throws Exception;
+
+  /**
+   * Author: Sanero.
+   * Created date: Feb 22, 2019
+   * Created time: 11:13:23 AM
+   * Description: TODO - check empty question. if true. change status to draft.
+   * @param examId
+   * @return
+   */
+  public boolean isEmptyQuestionOfExam(String examId);
 }
