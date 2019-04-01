50adb8af5d424bc7d4951333c870918daf20781a
anhmihanh97 <45336241+anhmihanh97@users.noreply.github.com>
2019-04-01 09:27:05 +0700

    a

--- TestingSystem-backend/src/main/java/com/cmcglobal/utils/ConstanPage.java
+++
@@ -1,9 +0,0 @@
-package com.cmcglobal.utils;
-
-public class ConstanPage {
-
-	public static final String RES_API = "/api/auth";
-	public static final String RES_API_SIGNIN = "/signin";
-	public static final String RES_API_SIGNUP = "/signup";
-
-}
